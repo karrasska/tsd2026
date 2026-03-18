a58aa66a16da4f3295926aeda8cc61f9aaa175bc
karrasska <kinga.prosniewska@onet.pl>
2026-03-18 18:47:15 +0100

    (task1) sufficient

---
+++ 03-LINQ/GoldSavings.App/DataServices/GoldDataExporter.cs
@@ -0,0 +1,61 @@
+﻿using GoldSavings.App.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace GoldSavings.App.Services
+{
+	public static class GoldDataExporter
+	{
+		// Task 3
+		public static void SavePricesToXml(List<GoldPrice> prices, string filePath)
+		{
+			// Validation
+			if (string.IsNullOrWhiteSpace(filePath))
+			{
+				Console.WriteLine("Error: empty filepath.");
+				return;
+			}
+
+			if (prices == null || !prices.Any())
+			{
+				Console.WriteLine("Error: no data to save empty list).");
+				return;
+			}
+
+			// Try-catch to handle potential IO exceptions
+			try
+			{
+				XmlSerializer serializer = new XmlSerializer(typeof(List<GoldPrice>));
+
+				// overwite by default
+				using (StreamWriter writer = new StreamWriter(filePath))
+				{
+					serializer.Serialize(writer, prices);
+				}
+
+				Console.WriteLine($"\nSaved {prices.Count} records to: {filePath}");
+			}
+			catch (UnauthorizedAccessException)
+			{
+				Console.WriteLine($"\nError: No acces to save in: {filePath}");
+			}
+			catch (DirectoryNotFoundException)
+			{
+				Console.WriteLine($"\nError: No existing path: {filePath}");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"\nUnexpected error in saving XML: {ex.Message}");
+
+			}
+		}
+
+		// Task 4
+		public static List<GoldPrice> ReadPricesFromXml(string filePath) => (List<GoldPrice>)new XmlSerializer(typeof(List<GoldPrice>)).Deserialize(new StreamReader(filePath));
+	}
+}
